8b9a2e62d5c7f1178de7761a585b66e6dc188387
SMUEric1127 <85500156+SMUEric1127@users.noreply.github.com>
2022-08-24 16:20:12 -0500

    added loop

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@ using namespace std;
 int main() {
 
     // add your code
-
+    for (int i = 9; i >= 1; i--) {
+        cout << i << endl;
+    }
+    cout << "It is time for data structures." << endl;
     return 0;
 }
